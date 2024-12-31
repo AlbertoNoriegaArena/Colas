67febe303acd23030099d108e17cb17117d8eae7
Alberto <alberto@gmail.com>
2024-12-31 09:58:39 +0100

    Ejercicio colas (FIFO)

---
+++ src/main/java/es/santander/ascender/Cola.java
@@ -0,0 +1,24 @@
+package es.santander.ascender;
+
+public class Cola {
+
+    private Nodo raiz;
+
+    public void add(String mensaje) {
+        if (raiz == null) {
+            this.raiz = new Nodo(mensaje);
+        } else {
+            this.raiz.pedirLaVez(mensaje);
+        }
+    }
+
+    public String get() {
+        if (raiz == null) {
+            return null;
+        } else {
+            String valor = raiz.getValor();
+            raiz = raiz.getSiguiente();
+            return valor;
+        }
+    }
+}
